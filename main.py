912d12d20654cc41405b542094f9bbd590376ba5
RobotReception <mo775451608@gmail.com>
2024-08-11 10:51:08 +0300

    first commit

---
+++ main.py
@@ -0,0 +1,7 @@
+echo "# GitFlow_test" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/Mo-Alshebli/GitFlow_test.git
+git push -u origin main
